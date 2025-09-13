8395c80ad94de2fe04775d5c1879ccc74708e600
JohnJohnsonTaren <tytar.den@gmail.com>
2025-09-13 15:12:03 +0300

    https://github.com/JohnJohnsonTaren/Module18-Dev.git

--- src/main/java/com/example/controller/NoteRestController.java
+++ src/main/java/com/example/controller/NoteRestController.java
@@ -1,9 +1,12 @@
 package com.example.controller;
 
+import com.example.dto.NoteRequestDto;
+import com.example.dto.NoteResponseDto;
 import com.example.entity.Note;
 import com.example.service.NoteService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import com.example.mapper.NoteMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,29 +17,32 @@ import java.util.List;
 @RequestMapping("/api/v1/notes")
 public class NoteRestController {
     private final NoteService noteService;
+    private final NoteMapper noteMapper;
 
     @GetMapping
-    public ResponseEntity<List<Note>> listAllNotes() {
-        return ResponseEntity.ok(noteService.listAll());
+    public ResponseEntity<List<NoteResponseDto>> listAllNotes() {
+        List<Note> notes = noteService.listAll();
+        return ResponseEntity.ok(noteMapper.toDtoList(notes));
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Note> getNoteById(@PathVariable Long id) {
+    public ResponseEntity<NoteResponseDto> getNoteById(@PathVariable Long id) {
         Note note = noteService.getById(id);
-        return ResponseEntity.ok(note);
+        return ResponseEntity.ok(noteMapper.toDto(note));
     }
 
     @PostMapping
-    public ResponseEntity<Note> createNote(@Valid @RequestBody Note note) {
-        Note createdNote = noteService.add(note);
-        return ResponseEntity.ok(createdNote);
+    public ResponseEntity<NoteResponseDto> createNote(@Valid @RequestBody NoteRequestDto noteDto) {
+        Note noteToCreate = noteMapper.toEntity(noteDto);
+        Note createdNote = noteService.add(noteToCreate);
+        return ResponseEntity.ok(noteMapper.toDto(createdNote));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Note> updateNote(@PathVariable Long id, @Valid @RequestBody Note note) {
-        note.setId(id);
-        Note updatedNote = noteService.update(note);
-        return ResponseEntity.ok(updatedNote);
+    public ResponseEntity<NoteResponseDto> updateNote(@PathVariable Long id, @Valid @RequestBody NoteRequestDto noteDto) {
+        Note noteToUpdate = noteMapper.toEntity(id, noteDto);
+        Note updatedNote = noteService.update(noteToUpdate);
+        return ResponseEntity.ok(noteMapper.toDto(updatedNote));
     }
 
     @DeleteMapping("/{id}")
